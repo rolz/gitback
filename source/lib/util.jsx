79c0b24142f92729ac3ca51366219a0b7395c60d
mayognaise <tobimayo@gmail.com>
2015-03-10 14:50:07 -0700

    added logger setup in util

--- source/lib/util.jsx
+++ source/lib/util.jsx
@@ -7,6 +7,22 @@ var logger = Logger.get('lib.util');
 var request = require('superagent');
 
 module.exports = {
+  setLogger() {
+    /*
+     * Logger setting
+     * https://www.npmjs.com/package/js-logger
+     */
+    Logger.useDefaults();
+
+    /*
+     * Only debug if you have "debug=true" in query
+     * https://www.npmjs.com/package/query-string
+     */
+    // if (qs.debug !== 'true') {
+    //   Logger.setLevel(Logger.WARN);
+    // }
+    Logger.setLevel(Logger.DEBUG);
+  },
   getUserData(userId, cb) {
     request.get(`/gitlogin/${userId}`, ((err, res) => {
       if (err) throw err;

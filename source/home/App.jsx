6b49d16de0f920800ff09f35f59590809cbea303
Abid Din <Abid.Din@akqa.com>
2015-03-17 17:24:30 -0700

    homepage styles/components

--- source/home/App.jsx
+++ source/home/App.jsx
@@ -1,16 +1,81 @@
 'use strict'
 
+var Intro = React.createClass({
+  render() {
+    return (
+      <section className="intro">
+        <a href="/" className="logo">GitBack</a>
+        <section className="tagline">
+          <p>Empowering <em>female developers</em> to code with every commit.</p>
+        </section>
+      </section>
+    );
+  }
+});
+
+var Feed = React.createClass({
+  render() {
+    return (
+      <section className="feed">
+        <ul>
+          <li className="user">
+            <img className="user-img" src="https://avatars2.githubusercontent.com/u/1561533?v=3&s=460" />
+            <div className="user-info">
+              <span className="user-contribution">Zac gave $0.4</span>
+              <span className="user-repo"><a href="#">rolz/gitback</a></span>
+            </div>
+            <span className="time-elapsed">6s ago</span>
+          </li>
+          <li className="user">
+            <img className="user-img" src="https://avatars3.githubusercontent.com/u/672890?v=3&s=460" />
+            <div className="user-info">
+              <span className="user-contribution">craftedpixelz gave $0.6</span>
+              <span className="user-repo"><a href="#">craftedpixelz/website</a></span>
+            </div>
+            <span className="time-elapsed">20s ago</span>
+          </li>
+          <li className="user">
+            <img className="user-img" src="https://avatars3.githubusercontent.com/u/588874?v=3&s=460" />
+            <div className="user-info">
+              <span className="user-contribution">mayognaise gave $0.10</span>
+              <span className="user-repo"><a href="#">mayognaise/ES6</a></span>
+            </div>
+            <span className="time-elapsed">52s ago</span>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+});
+
+var Login = React.createClass({
+  render() {
+    return (
+      <a href="/login" className="login-btn"><i className="icon"></i>Sign in with GitHub</a>
+    );
+  }
+});
+
+var Footer = React.createClass({
+  render() {
+    return (
+      <footer></footer>
+    );
+  }
+});
+
 var App = React.createClass({
   render() {
     return (
-      <div>
-        <a href="/" className="logo">GitBack</a>
-        <p>We’re uniting the dev community and teaching girls to code. Sign up with your GitHub account, and pledge donations for every commit you make. Let’s make the world a better place.</p>
-        <div className="total-raised">
-          <strong>$120.01</strong>
-          <small>raised so far</small>
+      <div className="container home">
+        <div className="overlay">
+          <div className="content">
+            <Intro/>
+            <Feed/>
+            <Login/>
+            <Footer/>
+          </div>
         </div>
-        <a href="/login">Login with GitHub</a>
       </div>
     );
   }

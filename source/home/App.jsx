bfe4f9231e5193b00096515bbd5f8e4768605c3b
Zac Rolland <zrolland@gmail.com>
2015-04-03 14:29:10 -0700

    homepage styling and feed

--- source/home/App.jsx
+++ source/home/App.jsx
@@ -5,69 +5,88 @@ var Hero = React.createClass({
 
     var self = this,
       {projectTitle, heroSection, sectionTwo} = this.props.context;
-
+      console.log(this.props.users);
     return (
       <section className="hero">
-        <section className="header">
-          <a href="/" className="logo">{projectTitle}</a>
-          <a href="#getInvolved" className="nav">{heroSection.nav[0]}</a>
-          <a href="#ourMission" className="nav">{heroSection.nav[1]}</a>
+        <section className="header clearfix">
+          <a className="logo" href="/">{projectTitle}</a>
+          <ul>
+            <li><a href="#getInvolved">{heroSection.nav[0]}</a></li>
+            <li><a href="#ourMission">{heroSection.nav[1]}</a></li>
+          </ul>
           <a href="/login" className="login">{heroSection.login}</a>
+        </section>
+        <section className="jumbotron">
+          <div className="tagline" dangerouslySetInnerHTML={{__html:heroSection.tagline}}></div>
+          <div className="userActions">
+            <a href="/login"><span className="signUp">{heroSection.signUp}<img src="/assets/images/github-logo.svg"/></span></a>
+          </div>
+        </section>
+        <section className="contribs">
+          <div className="header">
+            <div className="title">{heroSection.contribFeedTitle}</div>
+            <div className="total">$0.87</div>
+          </div>
+          <div className="terminal">
+            <div className="header">
+              <span className="ui-btn red"></span>
+              <span className="ui-btn yellow"></span>
+              <span className="ui-btn green"></span>
+            </div>
+
+            <Feed users={this.props.users} />
+
+          </div>
         </section>
       </section>
     );
   }
 });
 
-// var LiveFeed = React.createClass({
-//   render() {
-//     var rows = [];
-//
-//     this.props.users.forEach(function(user) {
-//       rows.push(<LiveFeedItem
-//         user={user}
-//         id={user.id}
-//         name={user.name}
-//         repoName={user.repo.name}
-//         repoUrl={user.repo.url}
-//         donation={user.donation}
-//         commiTime={user.lastCommit} />);
-//     }.bind(this));
-//
-//     return (
-//       <section className="feed">
-//         <ul>
-//           {rows}
-//         </ul>
-//       </section>
-//     );
-//   }
-// });
+var Feed = React.createClass({
+  render() {
+    var rows = [];
 
-// var LiveFeedItem = React.createClass({
-//   render() {
-//     var {username, repo, avatarUrl, createdAt, commits} = this.props.user;
-//     console.log(this.props.user);
-//     return (
-//       <li className="user">
-//         <img className="user-img" src={avatarUrl} />
-//         <div className="user-info">
-//           <p className="user-contribution">{username} gave ${commits.length * .01}</p>
-//           <p className="user-repo"><a href={`http://github.com/${username}/${repo}`}>{`${username}/${repo}`}</a></p>
-//         </div>
-//         <p className="time-elapsed">{(new Date(createdAt).toString())}</p>
-//       </li>
-//     )
-//   }
-// });
+    this.props.users.forEach(function(user) {
+      rows.push(<LiveFeedItem user={user} />);
+    }.bind(this));
 
-// var Login = React.createClass({
-//   render() {
-//     return (
-//       <a href="/login" className="login-btn"><i className="icon"></i>Sign in with GitHub</a>
-//     );
-//   }
-// });
+    return (
+      <section className="feed">
+        <ul>
+          {rows}
+        </ul>
+      </section>
+    );
+  }
+});
+
+var LiveFeedItem = React.createClass({
+  render() {
+    var {username, repo, avatarUrl, createdAt, commits} = this.props.user,
+    contribAmountPerPush = 0.01;
+
+    console.log(this.props.user);
+    return (
+      <li className="userContrib">
+        <span className="tbAvatar"><img src={avatarUrl} /></span>
+        <span className="username">{username}</span>
+        <span className="gave">gave</span>
+        <span className="amount">${contribAmountPerPush}</span>
+        <span className="repo"><a href={`http://github.com/${username}/${repo}`}>{`${username}/${repo}`}</a></span>
+        <span className="timeElapsed">{(new Date(createdAt).toString())}</span>
+      </li>
+    )
+  }
+});
+
+var Login = React.createClass({
+  render() {
+    return (
+      <a href="/login" className="login-btn"><i className="icon"></i>Sign in with GitHub</a>
+    );
+  }
+});
 
 var Footer = React.createClass({
   render() {
@@ -87,10 +106,10 @@ var App = React.createClass({
 
     return (
       <main className="home">
-          <div className="content">
-            <Hero context={context} />
-            <Footer/>
-          </div>
+
+        <Hero context={context} users={this.state.users} />
+        <Footer/>
+
       </main>
     );
   }
